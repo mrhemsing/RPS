460f7d8cc2d94e45ef61ec43f7c773db525fc362
Matt Hemsing <mhemsing@us.ibm.com>
2023-01-26 22:06:14 -0800

    Updates

--- src/script.js
+++ src/script.js
@@ -1,23 +1,27 @@
 //let a=document.createElement("canvas");
-let c = a.getContext("2d"), // no more $type conditional
+let c = a.getContext('2d'), // no more $type conditional
   w = window,
   d = document,
   M = Math,
   r = M.random,
   ruleSets = [
-    { id: 1, name: "🧱📄✂️  Rock, Paper, Scissors", rules: '✂️ cuts 📄 covers 🧱 crushes ✂️' }
+    {
+      id: 1,
+      name: '🪨📄✂️  Rock, Paper, Scissors',
+      rules: '✂️ cuts 📄 covers 🪨 crushes ✂️'
+    }
   ],
-  FPS = 30,//50fps
+  FPS = 30, //50fps
   SIZE = 0,
   PIECES_COUNT = 3 * 5 * 7 * 10,
   SPEED = 3,
-  TOUCH_DISTANCE = 3,
+  TOUCH_DISTANCE = 5,
   gameOn = false,
   targetMap = {},
   center = {},
   emojis = [],
   killFeed = [],
-  pieces = (new Array(90)).fill().map(() => ({ o: '', x: 0, y: 0 })), 
+  pieces = new Array(90).fill().map(() => ({ o: '', x: 0, y: 0 })),
   myInterval = null,
   gameRestartTimeout = null,
   gameStartTimeout = null,
@@ -25,15 +29,16 @@ let c = a.getContext("2d"), // no more $type conditional
 startButton = null;
 rounds = 1;
 victories_dict = {};
+
 let init = () => {
   w.addEventListener('resize', resize);
   resize();
-  ruleSets.map(r => r.rulesArr = r.rules.split(' '));
+  ruleSets.map(r => (r.rulesArr = r.rules.split(' ')));
   if (!myInterval) myInterval = setInterval(update, FPS);
   startButton = d.createElement('button');
-  startButton.id = "startButton";
-  startButton.innerHTML = "Start";
-  startButton.class = "startButton";
+  startButton.id = 'startButton';
+  startButton.innerHTML = 'Start';
+  startButton.class = 'startButton';
   start();
 };
 const initEmojis = () => {
@@ -46,7 +51,7 @@ const initEmojis = () => {
     if (i < size - 1) targetMap[emoji].push(selected.rulesArr[i + 2]);
   }
   emojis = Object.keys(targetMap);
-}; 
+};
 const start = () => {
   initEmojis();
   let o;
@@ -54,97 +59,111 @@ const start = () => {
     o = pieces[i];
     o.o = emojis[i % emojis.length];
     o.x = r() * innerWidth;
-    o.y = r() * innerHeight
-  } 
+    o.y = r() * innerHeight;
+  }
   killFeed = [];
-  if (gameRestartTimeout) clearTimeout(gameRestartTimeout); 
+  if (gameRestartTimeout) clearTimeout(gameRestartTimeout);
   if (gameStartTimeout) clearTimeout(gameStartTimeout);
   resize();
   gameOn = false;
 
   if (rounds == 1) {
     tempY = center.y - SIZE * 2;
-    write(
-      selected.name,
-      center.x,
-      tempY + SIZE * 1.5,
-      SIZE * 1,
-      true
-    );
+    write(selected.name, center.x, tempY + SIZE * 1.5, SIZE * 1, true);
   } else {
-    let text = "";
-    let count = ''
-    emojis.forEach((emoji) => {
+    let text = '';
+    let count = '';
+    emojis.forEach(emoji => {
       if (victories_dict[emoji]) {
         text += emoji;
-        count += victories_dict[emoji]
-
+        count += victories_dict[emoji];
       } else {
         text += emoji;
         count += 0;
       }
-      text += "\n";
-      count += "\n";
+      text += '\n';
+      count += '\n';
     });
     clear();
 
-    text.split("\n").forEach((sc, index) => {
+    text.split('\n').forEach((sc, index) => {
       write(
         sc,
         center.x - window.innerHeight / 4 + index * 180,
         center.y - SIZE * 2,
         SIZE * 3,
-        c.fillStyle = 'white',
+        (c.fillStyle = 'white'),
         true
       );
     });
-    count.split("\n").forEach((x, index) => {
+    count.split('\n').forEach((x, index) => {
       if (index <= 2) {
         write(
           x,
           center.x - window.innerHeight / 4 + index * 180,
           center.y + SIZE * 2,
           SIZE * 3,
-          c.fillStyle = 'white',
+          (c.fillStyle = 'white'),
           true
         );
       }
-    })
+    });
   }
   setTimeout(() => {
-    c.fillStyle = "#0000";
+    c.fillStyle = '#0000';
     clear();
-    write("Round " + rounds, center.x, center.y, SIZE * 2, c.fillStyle = 'white', true);
+    write(
+      'Round ' + rounds,
+      center.x,
+      center.y,
+      SIZE * 2,
+      (c.fillStyle = 'white'),
+      true
+    );
     temp_div.appendChild(startButton);
   }, 3000);
 
   startButton.addEventListener('click', () => {
-    startButton.innerHTML = "Starting...";
+    startButton.innerHTML = 'Starting...';
     gameStartTimeout = setTimeout(() => {
       gameOn = true;
       startButton.remove();
-      startButton.innerHTML = "Start";
+      startButton.innerHTML = 'Start';
     }, 1000);
-  })
+  });
 };
 let pieceMap = {};
 let drawScore = (o, y) => {
-  c.fillStyle = "white";
+  c.fillStyle = 'white';
   c.fillText(o + ' ' + (pieceMap[o] ? pieceMap[o].length : 0), SIZE, y);
 };
-let isTarget = (p1, p2) => p2.o && targetMap[p1.o] && targetMap[p1.o].includes(p2.o);
-let t = Date.now(), elapsed = 0, targets, weakness, closest, pangle, winIndex, tempY;
+let isTarget = (p1, p2) =>
+  p2.o && targetMap[p1.o] && targetMap[p1.o].includes(p2.o);
+let t = Date.now(),
+  elapsed = 0,
+  targets,
+  weakness,
+  closest,
+  pangle,
+  winIndex,
+  tempY;
 let update = () => {
   if (!gameOn) return;
   elapsed = Date.now() - t;
   clear();
-  pieces.sort((a, b) => { a.y - b.y });
+  pieces.sort((a, b) => {
+    a.y - b.y;
+  });
   pieces.map(p => {
     if (!p.o) return;
     //render
-    c.fillStyle = "white";
-    c.font = "30px serif";
-    c.fillText(p.o, p.x - SIZE / 2, p.y + SIZE / 2 + ((elapsed + emojis.indexOf(p.o)) % 5));
+    c.fillStyle = 'white';
+    c.font = '30px serif';
+    c.fillText(
+      p.o,
+      p.x - SIZE / 2,
+      p.y + SIZE / 2 + ((elapsed + emojis.indexOf(p.o)) % 5)
+    );
 
     //find closest target piece
     targets = pieces.filter(p2 => isTarget(p, p2));
@@ -157,25 +176,25 @@ let update = () => {
 
       // collision dection
       if (dist(p, closest) < TOUCH_DISTANCE) {
-        winIndex = selected.rulesArr.indexOf(p.o, selected.rulesArr.indexOf(closest.o));
+        winIndex = selected.rulesArr.indexOf(
+          p.o,
+          selected.rulesArr.indexOf(closest.o)
+        );
         killFeed.unshift(getFeed(p.o, closest.o));
         closest.o = p.o;
-        if (p.o === "🧱") {
-          var audio = new Audio("./assets/rock_sound.mpeg");
+        if (p.o === '🪨') {
+          var audio = new Audio('./assets/rock_sound.mpeg');
           audio.play();
-        }
-        else if (p.o === "📄") {
-          var audio = new Audio("./assets/paper_sound.mpeg");
+        } else if (p.o === '📄') {
+          var audio = new Audio('./assets/paper_sound.mpeg');
           audio.play();
-        }
-        else if (p.o === "✂️") {
-          var audio = new Audio("./assets/scissors_sound.mpeg");
+        } else if (p.o === '✂️') {
+          var audio = new Audio('./assets/scissors_sound.mpeg');
           audio.play();
         }
         //c.fillText(selected.collision || '💥', p.x - SIZE / 2, p.y + SIZE / 2);
       }
-    }
-    else {
+    } else {
       //if no targets, run away from their weakness! ...at 1/3rd the speed
       weakness = pieces.filter(p2 => !isTarget(p, p2));
       if (weakness.length > 0) {
@@ -192,7 +211,7 @@ let update = () => {
   tempY = 0;
   emojis.map((o, i) => {
     pieceMap[o] = pieces.filter(p => p.o === o);
-    tempY = (SIZE) + (i * SIZE * 1.2);
+    tempY = SIZE + i * SIZE * 1.2;
     //drawScore(o, tempY);
   });
   tempY += SIZE * 1.2;
@@ -206,7 +225,8 @@ let update = () => {
     gameOn = false;
   }
 };
-let getFeed = (a, b) => { //
+let getFeed = (a, b) => {
+  //
   let stop = false;
   let startIndex = 0;
   while (!stop) {
@@ -219,25 +239,41 @@ let getFeed = (a, b) => { //
   }
   return `${a} defeated ${b}`;
 };
-let renderKillFeed = () => killFeed.map((o, i) => write(o, SIZE / 2, tempY + (i * SIZE), SIZE * .7));
+let renderKillFeed = () =>
+  killFeed.map((o, i) => write(o, SIZE / 2, tempY + i * SIZE, SIZE * 0.7));
 let isEndGame = () => emojis.filter(o => didWin(o)).length === 1;
 let isDead = o => !pieceMap[o] || pieceMap[o].length === 0;
 let didWin = p => {
   if (emojis.filter(o => o !== p && isDead(o)).length === emojis.length - 1) {
     clear();
-    write(p + ' WINS', center.x, center.y, SIZE * 3, c.fillStyle = 'white', true);
+    write(
+      p + ' WINS',
+      center.x,
+      center.y,
+      SIZE * 3,
+      (c.fillStyle = 'white'),
+      true
+    );
     rounds += 1;
     if (Object.keys(victories_dict).includes(p)) {
       victories_dict[p] += 1;
-    }
-    else {
+    } else {
       victories_dict[p] = 1;
     }
+
     return true;
   }
+  updateWinCountInHeader(victories_dict);
   return false;
 };
-
+const updateWinCountInHeader = (dict = {}) => {
+  const el = document.getElementById('wincount');
+  let text = `🪨: ${dict['🪨'] || 0} 📄:${dict['📄'] || 0} ✂️: ${
+    dict['✂️'] || 0
+  }`;
+  el.innerHTML = text;
+};
+updateWinCountInHeader();
 onload = () => init();
 
 //---------------------------------------------------
@@ -249,11 +285,11 @@ const resize = () => {
   a.height = innerHeight;
   SIZE = M.min(a.width, a.height) / 15;
   c.font = SIZE + 'px serif';
-  center.x = innerWidth / 2
+  center.x = innerWidth / 2;
   center.y = innerHeight / 2;
 };
 let clear = () => {
-  c.fillStyle = "#000";
+  c.fillStyle = '#000';
   c.rect(0, 0, innerWidth, innerHeight);
   c.fill();
 };
